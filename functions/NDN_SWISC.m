aef5b8a58f6afa0e75b6d10bc26a64d9936c0c03
yjj <buguojj@gmail.com>
2025-03-19 22:41:52 +0800

    feat: 1. 修改SWISC的结果保存路径。 2. 内存性能优化 3. 增加variability结果

--- functions/NDN_SWISC.m
+++ functions/NDN_SWISC.m
@@ -57,9 +57,11 @@ end
 [datlas, ~] = NDN_Read(grayMatterMask);
 datlasR = reshape(datlas, [size(datlas,1) * size(datlas,2) * size(datlas,3), 1]);
 
-savepath_mat = [savedDir, '/mat/win_', num2str(wsize) '_sigma_' num2str(sigma)];
-savepath_nii = [savedDir, '/nii/win_', num2str(wsize) '_sigma_' num2str(sigma)];
-savepath_meanNii = [savedDir, '/niiMean/win_', num2str(wsize) '_sigma_' num2str(sigma)];
+savepath_mat = [savedDir, '/mat'];
+savepath_nii = [savedDir, '/nii'];
+savepath_meanNii = [savedDir, '/niiMean'];
+savepath_variabilityNii = [savedDir, '/niiVariability'];
+
 if exist(savepath_mat, 'dir') == 0
     mkdir(savepath_mat);
 end
@@ -69,13 +71,19 @@ end
 if  exist(savepath_meanNii, 'dir') == 0 
     mkdir(savepath_meanNii);
 end
+if  exist(savepath_variabilityNii, 'dir') == 0 
+    mkdir(savepath_variabilityNii);
+end
 
 
 %% 1. load all subjects in BOLD(nVoxel, nT, nSub)
 
 % get BOLD
-[BOLD, ~, ~] = getTimeCourse(inputdir, prefix, grayMatterMask, app);
-
+if nargin == 6
+    [BOLD, ~, ~] = getTimeCourse(inputdir, prefix, grayMatterMask, app);
+else
+    [BOLD, ~, ~] = getTimeCourse(inputdir, prefix, grayMatterMask);
+end
 % get nT nSub nR h
 sublist = getSublistByPrefixed(inputdir, prefix);
 if ~exist([inputdir filesep sublist(1).name filesep 'func'])
@@ -105,14 +113,14 @@ if nargin == 6
 end
 
 %% 2. save LOO mean TC in LOO_Mean_BOLD(nVoxel, nT, nSub)
-parfor subNum = 1:numel(sublist)
-    tmp = BOLD;
-    tmp(:, :, subNum) = [];
-    meanTmp=mean(tmp, 3);
-    LOO_Mean_BOLD(:, :, subNum) = meanTmp;
-end
-clear("meanTmp") % for saving storage
-clear("tmp")
+% parfor subNum = 1:numel(sublist)
+%     tmp = BOLD;
+%     tmp(:, :, subNum) = [];
+%     meanTmp=mean(tmp, 3);
+%     LOO_Mean_BOLD(:, :, subNum) = meanTmp;
+% end
+% clear("meanTmp") % for saving storage
+% clear("tmp")
 
 %% 3. compute sliding window
 disp('Create sliding window time series');
@@ -153,7 +161,19 @@ for suba = 1:numel(sublist)
 
     % 4.2 get the corresponding LOO_Mean_BOLD data of suba, do the same as
     % the above
-    bts = squeeze(LOO_Mean_BOLD(:,:, suba));
+%     BOLD(:, :, subNum) = [];
+%     meanTmp=mean(BOLD, 3);
+%     LOO_Mean_BOLD(:, :, subNum) = meanTmp;
+%     bts = squeeze(LOO_Mean_BOLD(:,:, suba));
+
+    if suba == 1
+        bts = mean(BOLD(:, :, 2:numel(sublist)), 3);
+    elseif suba == numel(sublist)
+        bts = mean(BOLD(:, :, 1:end - 1), 3);
+    else
+        bts = mean(BOLD(:, :, [1:suba-1, suba + 1:numel(sublist)]), 3);
+    end
+
     bts = bts';
     temp_bts = bts;
     temp_bts = temp_bts - repmat(mean(temp_bts), size(temp_bts, 1), 1); 
@@ -199,7 +219,7 @@ for suba = 1:numel(sublist)
     [path, filename, ext] = fileparts(sublist(suba).name);
     %% 5. saving result
     % 5.1 save as mat file
-    save([savepath_mat,'/swisc_' filename '.mat'], 'swiscResult', '-v7.3');
+    save([savepath_mat '/swisc_' filename '.mat'], 'swiscResult', '-v7.3');
 
     % 5.2 save as nii file
     niiData=zeros([size(swiscResult,1), size(datlas,1) * size(datlas,2) * size(datlas,3)]);
@@ -207,17 +227,19 @@ for suba = 1:numel(sublist)
     if  exist(savepath_nii)==0
         mkdir(savepath_nii);
     end
-    niiname = [savepath_nii filesep  '/swisc_' filename '.nii'];
+    niiname = [savepath_nii '/swisc_' filename '.nii'];
 
     niiData = niiData';
     niiData = reshape(niiData,[size(datlas,1), size(datlas,2), size(datlas,3), size(swiscResult,1)]);
     NDN_Write(niiData, niiname, h);
 
     % 5.3 save mean nii file
-    meanNiiName = [savepath_meanNii filesep  '/swisc_meanNii_' filename '.nii'];
+    meanNiiName = [savepath_meanNii  '/swisc_meanNii_' filename '.nii'];
     NDN_Write(mean(niiData, 4), meanNiiName, h);
 
-
+    % 5.4 save variability nii file
+    variabilityNiiName = [savepath_variabilityNii  '/swisc_variabilityNii_' filename '.nii'];
+    NDN_Write(std(niiData, 0, 4), variabilityNiiName, h);
 
     if nargin == 6
         ph.XData = [0, suba / nSub, suba / nSub, 0];
